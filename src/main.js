477a688813feb90a64949ed73b57f73e3b285a67
antoneli04 <antoniali_4@hotmail.com>
2021-03-01 13:01:46 -0500

    main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 // Este es el punto de entrada de tu aplicacion
-/*
-import { myFunction } from './lib/index.js';
 
-myFunction();
-*/
+import { routes, rootDiv, onNavigate } from "./routes.js";
 
 window.onpopstate = () => {
   rootDiv.innerHTML = routes[window.location.pathname];
